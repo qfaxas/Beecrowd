3504fbe1519eeaca9f21a0c73b9c93d8ce169f3a
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-05-06 18:54:14 -0300

    Update 1020.c

--- 1020.c
+++ 1020.c
@@ -3,6 +3,6 @@
 int main(){
 	int tempo;
 	scanf("%d", &tempo);
-	printf("%d ano(s)\n%d mes(es)\n%d dia(s)", (tempo/365), ((tempo%365)/30), ((tempo%365)%30));
+	printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", (tempo/365), ((tempo%365)/30), ((tempo%365)%30));
 	return 0;
 }
